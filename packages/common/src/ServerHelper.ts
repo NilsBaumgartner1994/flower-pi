cb69c4ce30bd93bf04c16317bd6381fd99739e93
Nils <nilsbaumgartner1994@gmail.com>
2026-01-31 02:19:42 +0100

    Backend Config Updated

--- packages/common/src/ServerHelper.ts
+++ packages/common/src/ServerHelper.ts
@@ -4,18 +4,6 @@ export type ServerConfig = {
 
 export class ServerHelper {
   public static TEST_SERVER_CONFIG: ServerConfig = {
-    server_url: 'https://test.rocket-meals.de/rocket-meals/api',
-  };
-
-  public static SWOSY_SERVER_CONFIG: ServerConfig = {
-    server_url: 'https://swosy.rocket-meals.de/rocket-meals/api',
-  };
-
-  public static STUDI_FUTTER_SERVER_CONFIG: ServerConfig = {
-    server_url: 'https://studi-futter.rocket-meals.de/rocket-meals/api',
-  };
-
-  public static SERVER_CONFIG_MUENSTER: ServerConfig = {
-    server_url: 'https://muenster.rocket-meals.de/rocket-meals/api',
+    server_url: 'https://127.0.0.1/flower-pi/api',
   };
 }
