fe34c7f530e0b231c7b8519d199bda90d5a65ef7
Nils <nilsbaumgartner1994@gmail.com>
2026-01-31 10:52:56 +0100

    Fix Docker Install

--- Setup.sh
+++ Setup.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-bash "$SCRIPT_DIR/Setup_install_docker.sh"
+bash "$SCRIPT_DIR/apps/backend/install_docker.sh"
 bash "$SCRIPT_DIR/setup_sensor_installation.sh"
